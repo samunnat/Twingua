42a6744b1b8665e9a233c7edc17fd2ed42ed4beb
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2019-03-14 23:15:47 -0600

    Initial dataproc commit

---
+++ dataproc-jobs/LanguageCountDriver.java
@@ -0,0 +1,22 @@
+package twingua;
+
+import org.apache.hadoop.hbase.mapreduce.Export;
+import org.apache.hadoop.util.ProgramDriver;
+
+public class LanguageCountDriver {
+
+  public static void main(String[] args) {
+    ProgramDriver programDriver = new ProgramDriver();
+    int exitCode = -1;
+    try {
+      programDriver.addClass("languagecount-hbase", LanguageCountHBase.class,
+          "A map/reduce program that counts the language in the bounding box");
+
+      programDriver.driver(args);
+      exitCode = programDriver.run(args);
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+    System.exit(exitCode);
+  }
+}
---
+++ dataproc-jobs/QueryBigTable.java
@@ -0,0 +1,11 @@
+package twingua;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
