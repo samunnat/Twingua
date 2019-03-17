ba5a537c675147fb991ce912177adade7905ccfa
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2019-03-17 16:15:51 -0600

    Dataproc request code added. Not tested beacuse we need to setup the GCP stuff first

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const request = require("request");
 const express = require("express");
 const app = express();
-const googleapi = "https://dataproc.googleapis.com";
+const dataproc = require("@google-cloud/dataproc");
+const credentials = require("/Users/Ryan_Loi/Downloads/celtic-vent-231205-27929c26f65b.json");
+
+const client = new dataproc.v1.JobControllerClient({
+  credentials: credentials
+});
 
 // Homepage Route
 app.get("/", (req, res) => {
@@ -12,45 +17,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/languages", (req, res) => {
-  const resource =
-    "/v1/projects/celtic-vent-231205/regions/global/jobs:submit/";
-  const opts = {
-    url: `${googleapi}${resource}`,
-    json: true,
-    body: {
-      projectId: "celtic-vent-231205",
-      job: {
-        placement: {
-          clusterName: "celticventcluster"
-        },
-        reference: {
-          jobId: "job-07a4f20c"
-        },
-        hadoopJob: {
-          args: [
-            "wordcount-hbase",
-            "gs://lesv-big-public-data/books/book",
-            "gs://lesv-big-public-data/books/b10",
-            "gs://lesv-big-public-data/books/b100",
-            "gs://lesv-big-public-data/books/b1232",
-            "gs://lesv-big-public-data/books/b6130",
-            "WordCount-1552544215"
-          ],
-          mainClass:
-            "gs://testbucketcelticvent/google-cloud-dataproc-metainfo/84edc588-da8a-4bdf-881b-c395c5e76ba7/jobs/99e233bd0fd0439391b3573d605abdb6/staging/wordcount-mapreduce-0-SNAPSHOT-jar-with-dependencies.jar"
-        }
+  console.log("Called languages route");
+
+  const dataprocRequest = {
+    projectId: "celtic-vent-231205",
+    region: "global",
+    // https://cloud.google.com/nodejs/docs/reference/dataproc/0.4.x/google.cloud.dataproc.v1#.Job
+    job: {
+      placement: {
+        clusterName: "celticventcluster"
+      },
+      reference: {
+        jobId: "job-07a4f20c"
+      },
+      hadoopJob: {
+        args: [
+          "wordcount-hbase",
+          "gs://lesv-big-public-data/books/book",
+          "gs://lesv-big-public-data/books/b10",
+          "gs://lesv-big-public-data/books/b100",
+          "gs://lesv-big-public-data/books/b1232",
+          "gs://lesv-big-public-data/books/b6130",
+          "WordCount-1552544215"
+        ],
+        mainClass:
+          "gs://testbucketcelticvent/google-cloud-dataproc-metainfo/84edc588-da8a-4bdf-881b-c395c5e76ba7/jobs/99e233bd0fd0439391b3573d605abdb6/staging/wordcount-mapreduce-0-SNAPSHOT-jar-with-dependencies.jar"
       }
     }
   };
 
-  // TODO: Need to create a service worker and authenicate it for the request to be accepted
-  request.post(opts, function(err, response, body) {
-    console.log(`Error: ${err}`);
-    console.log(`Response: ${JSON.stringify(response)}`);
-    console.log(`Body: ${body}`);
-  });
+  client
+    .submitJob(request)
+    .then(responses => {
+      const response = responses[0];
+      console.log(`Response: ${JSON.stringify(response)}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 
-  console.log("Called languages route");
   const test = {
     hi: 2,
     hello: "string"
