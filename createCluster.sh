fa26f6eab5e11153a6d55a7bc81434b98f5bcf45
samunnat <sala6632@colorado.edu>
2019-04-07 21:59:43 -0600

    Decreased memory (quota limit), increased storage, added Kafka

--- createCluster.sh
+++ createCluster.sh
@@ -6,8 +6,10 @@ if [[ $# -ne 1 ]]; then
 fi
 
 gcloud dataproc clusters create $1 \
-    --subnet default --zone us-central1-a \
-    --master-machine-type n1-standard-4 --master-boot-disk-size 50 \
-    --initialization-actions gs://dataproc-initialization-actions/hbase/hbase.sh \
-    --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 50 \
+    --region us-west1 \
+    --subnet default --zone "" \
+    --master-machine-type n1-standard-2 --master-boot-disk-type pd-ssd --master-boot-disk-size 100 \
+    --initialization-actions gs://dataproc-initialization-actions/zookeeper/zookeeper.sh,gs://dataproc-initialization-actions/hbase/hbase.sh,gs://dataproc-initialization-actions/kafka/kafka.sh \
+    --metadata "run-on-master=true" \
+    --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 100 \
     --image-version 1.3-deb9 --project big-data-project-233100
