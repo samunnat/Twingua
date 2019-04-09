cde0bd8555a31499eff8fc410f0cfe21307d3fd6
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2019-04-09 15:28:39 -0600

    API Code for submitting a job. Currently there is only one for the BulkLoad job.

--- app.js
+++ app.js
@@ -4,71 +4,82 @@ const app = express();
 const dataproc = require("@google-cloud/dataproc");
 const credentials = require("/Users/Ryan_Loi/Downloads/big-data-project-233100-e78608d5e4c9.json");
 
-// const client = new dataproc.v1.JobControllerClient({
-//   credentials: credentials
-// });
-
-const client = new dataproc.v1.ClusterControllerClient({
-  credentials: credentials
+const client = new dataproc.v1.JobControllerClient({
+  credentials: credentials,
   // optional auth parameters.
+  servicePath: "us-west1-dataproc.googleapis.com"
 });
 
 // Homepage Route
 app.get("/", (req, res) => {
+  console.log("Called root route");
+
   res
     .status(200)
     .send("hello world")
     .end();
 });
 
+// BulkLoad Route
+//  - Uploads twitter json files from bucket into HBase
+app.get("/BulkLoad", (req, res) => {
+  console.log("Called BulkLoad route");
+
+  // Creating job request configuration to start BulkLoad
+  const dataprocRequest = {
+    // Request header
+    // https://cloud.google.com/nodejs/docs/reference/dataproc/0.4.x/google.cloud.dataproc.v1#.Job
+    projectId: "big-data-project-233100",
+    region: "us-west1",
+    job: {
+      placement: {
+        clusterName: "testcl"
+      },
+      hadoopJob: {
+        args: [
+          "com.twing.app.HBaseDriver",
+          "gs://dataproctst/datInput.xml",
+          "gs://dataproctst/hfiles",
+          "tweet"
+        ],
+        mainJarFileUri: "gs://dataproctst/hbase-bulkload-1.0.jar"
+      }
+    }
+  };
+
+  // TODO: Add in things to keep track of the job
+
+  // Submitting job request to dataproc
+  client
+    .submitJob(dataprocRequest)
+    .then(responses => {
+      console.log("Job Submitted");
+      res.send("Job Submitted");
+    })
+    .catch(err => console.log("Error:", err));
+});
+
+// Languages Route
+//  - Gets counts of each language within each bounding box
 app.get("/languages", (req, res) => {
   console.log("Called languages route");
-  // res.send(client.toString());
+
+  // Creating job request configuration to run Spark Job
   const dataprocRequest = {
-    projectId: "big-data-project-233100",
-    region: "global"
+    // Request header
     // https://cloud.google.com/nodejs/docs/reference/dataproc/0.4.x/google.cloud.dataproc.v1#.Job
-    // job: {
-    //   placement: {
-    //     clusterName: "testcl"
-    //   },
-    //   reference: {
-    //     jobId: "job-07a4f20c"
-    //   },
-    //   hadoopJob: {
-    //     args: ["wordcount-hbase", "gs://bigdata_tweet_dump"],
-    //     mainClass:
-    //       "gs://testbucketcelticvent/google-cloud-dataproc-metainfo/84edc588-da8a-4bdf-881b-c395c5e76ba7/jobs/99e233bd0fd0439391b3573d605abdb6/staging/wordcount-mapreduce-0-SNAPSHOT-jar-with-dependencies.jar"
-    //   }
-    // }
-  };
-  console.log(client);
-  client
-    .listClusters(dataprocRequest)
-    .then(responses => {
-      const resources = responses[0];
-      console.log("Total resources:", resources.length);
-      for (let i = 0; i < resources.length; i += 1) {
-        console.log(resources[i]);
+    projectId: "big-data-project-233100",
+    region: "us-west1",
+    job: {
+      placement: {
+        clusterName: "testcl"
+      },
+      SparkJob: {
+        args: [],
+        mainJarFileUri: ""
       }
-    })
-    .catch(err => console.log(err));
-
-  // client
-  //   .submitJob(request)
-  //   .then(responses => {
-  //     const response = responses[0];
-  //     console.log(`Response: ${JSON.stringify(response)}`);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //   });
-
-  // const test = {
-  //   hi: 2,
-  //   hello: "string"
-  // };
-  // res.json(test);
+    }
+  };
 });
 
 const PORT = process.env.PORT || 8080;
