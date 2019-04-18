cfbe826f777a3f0336cc6f2a7c4408df6d9528c0
samunnat <sala6632@colorado.edu>
2019-04-17 21:22:10 -0600

    added basic script to compile jar and submit job

--- dataproc-jobs/tweetAnalyzer/analyzer/compile.sh
+++ dataproc-jobs/tweetAnalyzer/analyzer/compile.sh
@@ -2,3 +2,4 @@
 
 sbt compile
 sbt package
+gcloud dataproc jobs submit spark --cluster testcl --region us-west1 --jar target/scala-2.11/analyzer.jar --jars gs://dataproctst/shc-core-1.1.1-2.1-s_2.11.jar
