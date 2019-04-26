9717d8bd621841b40046d33e4bcc9d53a9e825a3
Ismael Garrido <100346215@alumnos.uc3m.es>
2019-04-26 14:30:01 -0600

    tweet streamer updated

--- tweet-stream.py
+++ tweet-stream.py
@@ -2,6 +2,7 @@ import tweepy
 import sys
 import time
 import os
+import requests
 
 # Variables that contains the user credentials to access Twitter API
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
@@ -13,13 +14,20 @@ CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 # print(os.environ)
 
 # output_file = 'tweets_eu.json'
-output_file2 = 'tweets_eu_3.json'
+global output_file2
+output_file2 = 'tweets_1.json'
+
+# Size of the file 100000000
+file_max_size = 100000000
+
+# iterator to generate new files with different names
+file_iterator = 1
 
 # Mapbox done at http://boundingbox.klokantech.com/
-GEO_EUROPE_BOX = [-24.64501397,35.8630349534,45.1656662867,71.2702519386]
+GEO_EUROPE_BOX = [-24.64501397, 35.8630349534, 45.1656662867, 71.2702519386]
 
 # Languages in Europe
-LANGUAGES = ["es, en, cs, da, de, el, fi, fr, he, hu, it, nl, no, pl, pt, ro, ru, sv, tr, uk"]
+# LANGUAGES = ["es, en, cs, da, de, el, fi, fr, he, hu, it, nl, no, pl, pt, ro, ru, sv, tr, uk"]
 
 # initialize a list to hold all the tweepy Tweets
 alltweets = []
@@ -33,9 +41,25 @@ class CustomStreamListener(tweepy.StreamListener):
     # Write the tweets on the JSON file
     def on_data(self, data):
         # if file is bigger than 90 MB exit the program
-        if os.path.getsize(output_file2) > 90000000:
-            print "File is ", os.path.getsize(output_file2), ". Bigger than 90MB, exiting the program"
-            exit()
+        global output_file2
+        if os.path.getsize(output_file2) > file_max_size:
+            file.close()
+            print "File is ", os.path.getsize(output_file2), ". Bigger than 90MB, generating a new one"
+            gcs_destination = 'gsutil cp ./' + output_file2 + ' gs://bigdata_tweet_dump/'
+            os.system(gcs_destination)
+            os.system('rm ' + output_file2)
+            endpoint = 'https://big-data-project-233100.appspot.com/bulkload?filename=tweets_' + str(file_iterator)
+            print "API request: " + endpoint
+            r = requests.get(endpoint)
+            # print r.status_code
+            # print "-------------"
+            # print r.json
+            global file_iterator
+            file_iterator = file_iterator + 1
+            output_file2 = 'tweets_' + str(file_iterator) + '.json'
+            global file
+            file = open(output_file2, 'a')
+            print "Opening file: ", output_file2
         file.write(data)
 
     def on_error(self, status_code):
@@ -46,13 +70,19 @@ class CustomStreamListener(tweepy.StreamListener):
         print >> sys.stderr, 'Timeout...'
         return True # Don't kill the stream
 
-# authenticate access to Twitter API
+
+# class MyStreamListener(tweepy.StreamListener):
+#    def on_status(self, status):
+#        print(status.text)
+
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
-# open output file of the downloaded tweets
+global file
 file = open(output_file2, 'a')
+counter3 = 0
 
 # Error handling
 if not api:
@@ -61,18 +91,14 @@ if not api:
 # myStreamListener = MyStreamListener()
 # myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
+# counter = 0
 print("Starting Streaming filter")
 
-# Start Streaming filter
 stream = tweepy.streaming.Stream(auth, CustomStreamListener())
-
-# filter tweets by set of languages and boundary box around Europe
-stream.filter(languages=LANGUAGES, locations=GEO_EUROPE_BOX)
-
-# Let the program sleep if we reach the maximum number of requests
-time.sleep(10)
+stream.filter(locations=GEO_EUROPE_BOX)
 
 print("Done with the filtering")
+
 
 # new_tweets = myStream.filter(languages=LANGUAGES, locations=GEO_EUROPE_BOX, count=["10"])
 # print(json.dumps(myStream.filter(locations=GEO_EUROPE_BOX)))
@@ -86,3 +112,6 @@ for tweet in new_tweets:
         with open('tweets_eu.json', 'w', encoding='utf8') as file:
             json.dump(tweet._json, file)
 '''
+
+
+
