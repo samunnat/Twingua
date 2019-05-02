8c06c4f85c1655f2d6ed9aa8c34609f16cb29617
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2019-05-02 01:35:47 -0600

    initial socketio code with skeleton listeners

--- frontend/src/components/containers/map.jsx
+++ frontend/src/components/containers/map.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Leaflet from "leaflet";
 import RadioButton from "../atoms/radioButton.jsx";
-
+import socketIO from "socket.io-client";
 import { stylesListToClassNames } from "../../lib/utils";
 
 const classes = stylesListToClassNames({
@@ -24,54 +24,58 @@ const classes = stylesListToClassNames({
   }
 });
 
+const colors = [
+  "#EF9A9A",
+  "#F44336",
+  "#880E4F",
+  "#F48FB1",
+  "#AB47BC",
+  "#7B1FA2",
+  "#283593",
+  "#9FA8DA",
+  "#1565C0",
+  "#90CAF9",
+  "#0288D1",
+  "#81D4FA",
+  "#80DEEA",
+  "#00897B",
+  "#80CBC4",
+  "#A5D6A7",
+  "#43A047",
+  "#FFEE58",
+  "#FFA000",
+  "#FFE0B2",
+  "#FF5722",
+  "#D84315",
+  "#BCAAA4",
+  "#795548",
+  "#37474F",
+  "#B0BEC5",
+  "#004D40",
+  "#827717",
+  "#880E4F",
+  "#FF6F00",
+  "#B71C1C",
+  "#E8F5E9",
+  "#E8EAF6",
+  "#FFEBEE",
+  "#212121"
+];
+
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       radioValue: "label"
     };
-
-    // this.colors = {}
-    this.colors = [
-      "#EF9A9A",
-      "#F44336",
-      "#880E4F",
-      "#F48FB1",
-      "#AB47BC",
-      "#7B1FA2",
-      "#283593",
-      "#9FA8DA",
-      "#1565C0",
-      "#90CAF9",
-      "#0288D1",
-      "#81D4FA",
-      "#80DEEA",
-      "#00897B",
-      "#80CBC4",
-      "#A5D6A7",
-      "#43A047",
-      "#FFEE58",
-      "#FFA000",
-      "#FFE0B2",
-      "#FF5722",
-      "#D84315",
-      "#BCAAA4",
-      "#795548",
-      "#37474F",
-      "#B0BEC5",
-      "#004D40",
-      "#827717",
-      "#880E4F",
-      "#FF6F00",
-      "#B71C1C",
-      "#E8F5E9",
-      "#E8EAF6",
-      "#FFEBEE",
-      "#212121"
-    ];
   }
 
   componentDidMount() {
+    this.socket = socketIO("http://localhost:4000");
+    this.socket.on("return-languages", this.addPolygon);
+    // this.socket.on("connect", function(socket) {
+    //   console.log("Connected!");
+    // });
     // Creating the map
     // NOTE: Might want to add bounds to the map
     this.layers = {
@@ -99,50 +103,40 @@ class Map extends React.Component {
     });
 
     // Map zoom handler
-    this.map.on("zoomend", this.zoomHandler);
-
-    // Adding bounding boxes
-    const incrementAmt = 0.1;
-    const end = 5;
-    this.boundingBoxes = new Array(end / incrementAmt);
-    for (let i = 0; i < end; i += incrementAmt) {
-      this.boundingBoxes[i] = [];
-
-      for (let j = 0; j < end; j += incrementAmt) {
-        this.boundingBoxes[i].push(
-          Leaflet.polygon(
-            // [[0, 0], [0, 1], [1, 1], [1, 0]],
-            [
-              [45 + i, j],
-              [45 + i, incrementAmt + j],
-              [45 + incrementAmt + i, incrementAmt + j],
-              [45 + incrementAmt + i, j]
-            ],
-            {
-              color: this.colors[
-                Math.floor(Math.random() * this.colors.length)
-              ],
-              fillOpacity: 0.2
-            }
-          ).addTo(this.map)
-        );
-      }
-    }
-
-    // Adding a polygon to the map
-    // var polygon = Leaflet.polygon(
-    //   [[47.526, -1.5], [46.526, -1.5], [46.526, -0.0], [47.526, -0.0]],
-    //   { color: "red" }
-    // ).addTo(this.map);
-
-    // Changing the color of a bounding box
-    // this.boundingBoxes[1][1].setStyle({
-    //   fillColor: "#4c4c4c",
-    //   fillOpacity: 0.4
-    // });
+    this.map.on("moveend", this.zoomHandler); // This handles moving and zoom
   }
 
+  addPolygon = jsonObj => {
+    console.log("server says hello world");
+    // const key = Object.keys(jsonObj)[0];
+    // const coords = key.split(",").map(ele => parseFloat(ele));
+    // const languages = Object.keys(jsonObj[key]);
+    // var max = (-1, -1);
+    // languages.forEach((count, index) => {
+    //   if (count > max[1]) {
+    //     max = (index, count);
+    //   }
+    // });
+
+    // if (max[0] !== -1) {
+    //   Leaflet.polygon(
+    //     [
+    //       [coords[0], coords[1]], // lat1, long1
+    //       [coords[0], coords[3]], // lat1, long2
+    //       [coords[2], coords[3]], // lat2, long2
+    //       [coords[2], coords[1]] // lat2, long1
+    //     ],
+    //     {
+    //       color: colors[Math.floor(Math.random() * colors.length)],
+    //       fillOpacity: 0.2
+    //     }
+    //   ).addTo(this.map);
+    // }
+  };
+
   zoomHandler = e => {
+    this.socket.emit("get-languages", "hello world");
+
     // Call API to query bounding box information
     var precision;
     switch (e.target._zoom) {
@@ -162,15 +156,19 @@ class Map extends React.Component {
 
     console.log("Zoom level: " + e.target._zoom);
     const bounds = this.map.getBounds();
-
+    const SW = { lat: bounds._southWest.lat, lng: bounds._southWest.lng };
+    const NE = { lat: bounds._northEast.lat, lng: bounds._northEast.lng };
     // TESTING
-    precision = 6;
+    precision = 4;
+
+    this.updateBBoxes(SW, NE, precision);
+  };
+
+  updateBBoxes = (SW, NE, precision) => {
     fetch(
-      `http://localhost:3000/languages?lat1=${bounds._southWest.lat}&long1=${
-        bounds._southWest.lng
-      }&lat2=${bounds._northEast.lat}&long2=${
-        bounds._northEast.lng
-      }&precision=${precision}`,
+      `http://localhost:4000/languages?lat1=${SW.lat}&long1=${SW.lng}&lat2=${
+        NE.lat
+      }&long2=${NE.lng}&precision=${precision}`,
       {
         headers: {
           "Content-Type": "application/json"
@@ -200,9 +198,7 @@ class Map extends React.Component {
                 [coords[2], coords[1]] // lat2, long1
               ],
               {
-                color: this.colors[
-                  Math.floor(Math.random() * this.colors.length)
-                ],
+                color: colors[Math.floor(Math.random() * colors.length)],
                 fillOpacity: 0.2
               }
             ).addTo(this.map);
@@ -216,7 +212,7 @@ class Map extends React.Component {
 
   clearMap = () => {
     for (var i in this.map._layers) {
-      if (this.map._layers[i]._path != undefined) {
+      if (this.map._layers[i]._path !== undefined) {
         try {
           this.map.removeLayer(this.map._layers[i]);
         } catch (e) {
